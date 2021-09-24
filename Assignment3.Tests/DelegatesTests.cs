a9c70eab7ae097ecf1d0b8415b39960a410df0c2
Trindkr <maria.trindkaer@gmail.com>
2021-09-24 12:37:05 +0200

    fixed namespaces

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,14 +1,14 @@
 using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+namespace BDSA2021.Assignment03.Tests
 {
     public class DelegatesTests
     {
         [Fact]
         public void Test1()
         {
-
+ 
         }
     }
 }
