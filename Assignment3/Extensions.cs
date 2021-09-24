0cd16e1f256012f7f80fe01e95ce4a8f4b54f5bd
Trindkr <maria.trindkaer@gmail.com>
2021-09-24 13:15:37 +0200

    exercise 1.0

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 namespace BDSA2021.Assignment03
 {
     public static class Extensions
     {
+        public static IEnumerable<int> Flatten(this IEnumerable<IEnumerable<int>> items)
+        {
+            foreach (var item in items)
+            {
+                foreach (var i in item)
+                {
+                    yield return i;
+                }
+            }
+        }
+        public static IEnumerable<int> Filter(this IEnumerable<int> items, Predicate<int> predicate)
+        {
+            foreach (var i in items)
+            {
+                if(predicate(i))
+                {   
+                    yield return i;
+                }
+            }
+        }   
     }
 }
