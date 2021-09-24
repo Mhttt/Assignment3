0e57d1c02adb81600d84862b2ba39e9631ad13d4
Trindkr <mtot@itu.dk>
2021-09-24 21:15:26 +0200

    implemented extention methods using linq

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -9,68 +9,32 @@ namespace BDSA2021.Assignment03.Tests
         [Fact]
         public void Flatten_Given2DIntArray_ReturnsIntArray()
         {
-            //Given
             var subInput1 = new int[]{1,2,3,4,5,6};
             var subInput2 = new int[]{6,5,4,3,2,1};
             var input = new int[][]{subInput1,subInput2};
             
-            //When
             var output = input.Flatten();
-
-            //Then
             Assert.Equal(new int[]{1,2,3,4,5,6,6,5,4,3,2,1},output);
 
         }
 
         [Fact]
-        public void Filter_given3_4_5_6_2_8_6_49Predicate_returns49()
+        public void Filter_given3_4_5_6_2_8_6_49_returns49()
         {
-            //Given
-            Predicate<int> predicate = divisibleBy7_greaterThan42;
-
-            bool divisibleBy7_greaterThan42(int n) => n %7 == 0 && n>42; 
-
             var input = new int[]{3,4,5,6,2,8,6,49};
-            
-            //When
-            var output = input.Filter(predicate);
-            
-            //Then
+            var output = input.Filter();
             Assert.Equal(new int[]{49}, output);
-        
         }
 
         [Fact]
         public void Filter_given_200_33_3211_400_800_and_predicateLeapYea_returns400_800()
         {
-            //Given
+
             var input = new int[]{200, 33, 3211, 400, 800};
 
-            Predicate<int> predicateLeapYear = isLeapYear;
-            bool isLeapYear(int year)
-            {
-                if(year%400==0)
-                {
-                    return true;
-                }
-                else if(year%100==0)
-                {
-                    return false;
-                }
-                else if(year%4==0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            };
-
-            //When
-            var output = input.Filter(predicateLeapYear);
-            //Then
-            Assert.Equal(new int[]{400,800}, output);
+            var output = input.LeapYear();
+            
+            Assert.Equal(new int[]{400,800}, output); 
         }
 
         
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,43 +1,21 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace BDSA2021.Assignment03
 {
     public static class Extensions
     {
-        public static IEnumerable<int> Flatten(this IEnumerable<IEnumerable<int>> items)
-        {
-            foreach (var item in items)
-            {
-                foreach (var i in item)
-                {
-                    yield return i;
-                }
-            }
-        }
-        public static IEnumerable<int> Filter(this IEnumerable<int> items, Predicate<int> predicate)
-        {
-            foreach (var i in items)
-            {
-                if(predicate(i))
-                {   
-                    yield return i;
-                }
-            }
-        }   
+        public static IEnumerable<int> Flatten(this IEnumerable<IEnumerable<int>> xs) => xs.SelectMany(x=>x);
+       
+        public static IEnumerable<int> Filter(this IEnumerable<int> yx) => from y in yx where y >42&& y%7==0 select y;
 
-        public static bool IsSecure(this Uri uri)
-        {
-            return uri.Scheme == "https";
-        }
+        public static IEnumerable<int> LeapYear(this IEnumerable<int> yx) => from y in yx where DateTime.IsLeapYear(y) select y;
+                                                                            
+        public static bool IsSecure(this Uri uri) => uri.Scheme == "https";
 
-        public static int WordCount(this string s)
-        {
-            return (Regex.Split(s,@"\P{L}+")).Count();
-
-        }
+        public static int WordCount(this string s) => (Regex.Split(s,@"\P{L}+")).Count();
 
         public static void Print(this char[] c)
         {
